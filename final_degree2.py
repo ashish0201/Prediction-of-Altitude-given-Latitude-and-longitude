c6f226c2056c9e24391b4a89b380eb08174fb1da
ASHISH GUPTA <ashishggupta0201@gmail.com>
2020-03-20 12:02:00 +0530

    Add files via upload

---
+++ final_degree2.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 15:39:24 2019
+
+@author: hp
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from mpl_toolkits.mplot3d import Axes3D  
+file = open("3D_spatial_network.txt",'r')
+dataset = file.read()
+temp=dataset.split("\n")
+temp_len=len(temp)
+data = np.zeros((temp_len,4))
+
+for i in range(0,temp_len-1):
+    line=temp[i].split(",");
+    for j in range(0,4):
+        data[i][j]=float(line[j])
+   
+data=data[:,1:4]  
+result=data[:,2:3]
+data=data[:,0:2]
+
+#Appending ones
+data=np.append(arr=np.ones((len(data),1)),values=data,axis=1)#1
+data=np.append(arr=data[:,1:2]*data[:,2:3],values=data,axis=1)#xy
+data=np.append(arr=data[:,3:4]**2,values=data,axis=1)#y2
+data=np.append(arr=data[:,3:4]**2,values=data,axis=1)#x2
+
+traindata,testdata,trainresult,testresult=train_test_split(data,result,test_size=0.3)
+
+avg=[0]*6
+std=[0]*6
+for i in range(0,6):
+    avg[i]=np.mean(traindata[:,i:i+1])
+    std[i]=np.std(traindata[:,i:i+1])
+    if std[i]!=0:
+        traindata[:,i]=(traindata[:,i]-avg[i])/std[i]
+        testdata[:,i]=(testdata[:,i]-avg[i])/std[i]
+
+for i in range(0,1):
+    avg[i]=np.mean(trainresult[:,i:i+1])
+    std[i]=np.std(trainresult[:,i:i+1])
+    if std[i]!=0:
+        trainresult[:,i]=(trainresult[:,i]-avg[i])/std[i]
+        testresult[:,i]=(testresult[:,i]-avg[i])/std[i]
+
+#-----------------------------
+y_bar=0
+for i in range(0,len(trainresult[:,0])):
+    y_bar+=trainresult[i][0]
+aa=np.std(trainresult[:,0])
+y_bar/=len(trainresult[:,0])
+sst=0
+for i in range(0,len(trainresult[:,0])):
+    sst+=(trainresult[i][0]-y_bar)**2
+sst/=2
+ 
+w=np.empty((1,6))
+w.fill(0)
+   #best-0.000002
+error_per_iteration=np.zeros((300,1))
+eta=9e-7
+for iterate in range(0,300):
+    print(iterate)
+    print(w)
+    sum_w=np.zeros((1,6))
+    for row in range(0,len(traindata)):
+        
+        for j in range(0,6):
+            sum_w[0][j]+=(traindata[row][j]*((np.dot(traindata[row],w.T))-trainresult[row][0]))
+        
+
+    for j in range(0,6):
+        w[0][j]-=eta*(sum_w[0][j])
+
+    
+    error_sum=0
+    for row in range(0,len(traindata)):
+        error_sum+=(((np.dot(traindata[row],w.T))-trainresult[row][0])**2)
+    error_sum/=2
+    print(error_sum)
+    error_per_iteration[iterate]=error_sum
+    sse=error_sum
+    r2=1-(sse/sst)
+    print(r2)
+    print("\n")
+
+store_err=error_per_iteration
+err=error_per_iteration[:300,:]
+iteration_num = [i for i in range(0,300)]
+plt.plot(iteration_num,err, color='g')
+plt.xlabel('Number of iterations')
+plt.ylabel('Error Loss')
+plt.title('Gradient Descent Graph')
+plt.show()
+# =============================================================================
+#     Final Iteration
+# 299
+# [[-2.99947258e-01 -2.81268190e-01  1.80239843e-01 -1.42212662e-15
+#    2.70786556e-01  8.76875653e-02]]
+# [147658.40894429]
+# [0.02987787]
+# 
+# =============================================================================
